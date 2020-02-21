cb81688b3e67b07c3a8c7386f95cb63fc48c92be
Tim Kessler <tkessler@consoloservices.com>
2020-02-21 09:14:53 -0500

    change to use api-url.txt

--- bin/demo.sh
+++ bin/demo.sh
@@ -1,12 +1,16 @@
-export API_URL='https://uwzjz947ok.execute-api.us-east-1.amazonaws.com/dev/api/v1/pdf/capture'
+#!/bin/bash
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+STARTDIR=$PWD
+
+cd $SCRIPTDIR
+
+export API_URL=$(<$SCRIPTDIR/../config/api-url.txt)
 export PARAM1=$1
 if [ "${PARAM1}" == "" ]
 then
 	export PARAM1="1"
 fi
-export DEMO_URL="https%3A%2F%2Fwww.google.com%2Fsearch?q=${PARAM1}&tbm=isch"
 export DEMO_URL="https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3D${PARAM1}%26tbm%3Disch"
-echo $DEMO_URL
 export TEMP_DIR='/tmp'
 export TEMP_FILE_NAME="output-${PARAM1}.pdf"
 
@@ -20,3 +24,4 @@ curl -o "${TEMP_DIR}/${TEMP_FILE_NAME}"  --location --request GET "${API_URL}?ur
 # Mac command to open in Chrome
 #/usr/bin/open -na "/Applications/Google Chrome.app" --args --new-window  "${TEMP_DIR}/${TEMP_FILE_NAME}"
 
+cd $STARTDIR
